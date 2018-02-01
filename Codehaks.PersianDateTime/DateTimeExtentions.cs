da98834c49d1d938e4279d3f6292628a2b9729eb
Hakim Ghods <ghods.hakim@hotmail.com>
2018-02-01 21:14:16 +0330

    Add project files.

---
+++ Codehaks.PersianDateTime/DateTimeExtentions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static Codehaks.PersianDateTime.PersianDateTime;
+
+namespace Codehaks.PersianDateTime
+{
+    public static class DateTimeExtentions
+    {
+        public static string ToPersianDate(this DateTime dateTime)
+        {
+            return PersianDateHelper.GetPersianDate(dateTime);
+        }
+
+        public static string ToPersianDateTime(this DateTime dateTime)
+        {
+            return PersianDateHelper.GetPersianDateTime(dateTime);
+        }
+
+        public static string ToFullPersianDateTime(this DateTime dateTime)
+        {
+            return PersianDateHelper.GetFullPersianDateTime(dateTime);
+        }
+
+        public static string ToFullPersianDate(this DateTime dateTime)
+        {
+            return PersianDateHelper.GetFullPersianDate(dateTime);
+        }
+    }
+}
---
+++ Codehaks.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Codehaks.PersianDateTime;
+
+namespace Codehaks.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ToPersianDate : " + DateTime.Now.ToPersianDate());
+            Console.WriteLine("ToPersianDateTime : " + DateTime.Now.ToPersianDateTime());
+            Console.WriteLine("ToFullPersianDate : " + DateTime.Now.ToFullPersianDate());
+            Console.WriteLine("ToFullPersianDateTime : " + DateTime.Now.ToFullPersianDateTime());
+
+            Console.ReadLine();
+        }
+    }
+}
